f80544133152cd401041e2b8a5252a7041099588
mziya <mziyag5187@gmail.com>
2022-12-15 22:03:11 +0300

    my last commit and my work is done

--- src/Ziya.java
+++ src/Ziya.java
@@ -2,6 +2,7 @@ public class Ziya {
     public static void main(String[] args) {
         System.out.println("my first change");
         System.out.println("my second change");
+        System.out.println("my last change");
 
     }
 }
